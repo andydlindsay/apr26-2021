06be7516e889493587cef14ae36c92d32ae78bdd
Andy Lindsay <andydlindsay@gmail.com>
2021-04-28 10:28:35 -0700

    lecture break

---
+++ w01d03/01_primitives.js
@@ -0,0 +1,19 @@
+// primitives in JS
+const nul = null;
+const undef = undefined;
+const str = 'string';
+const num = 42;
+const bool = true;
+
+const symbol = Symbol('my-symbol');
+const bigInt = 64892634986289n;
+
+// consist of single values
+// immutable === unchangeable
+
+let username = 'Alice';
+username = 'Bob';
+
+let greeting = 'hello world';
+greeting.replace('hello', 'hola');
+console.log(greeting);
---
+++ w01d03/02_data-structures.js
@@ -0,0 +1,49 @@
+// const student = 'Alice';
+const cohort = 'APR26';
+const avgGrade = 90;
+
+const studentOne = ['Alice', 'APR26', 90];
+// console.log(studentOne[300]);
+
+// const student = 'Bob';
+
+const student = {
+  names: ['Alice', 'Tom'],
+  cohort: 'APR26',
+  avgGrade: 90,
+  friend: {
+    name: 'Dean',
+    scores: [10, 15, 7]
+  },
+  myFunc: function() {}
+};
+
+// we access and set the values in the same way
+student.cohort = 'MAY24';
+
+// console.log(student);
+console.warn('just the same');
+console.info('hello there info');
+
+// const myFunc = function() {};
+
+// student.friend.scores.length
+
+// console.log()
+
+
+// console.log(student['names'][1]);
+
+// student.names.push('Carol');
+
+// const anythingIWant = 'graduationDate';
+
+// // square bracket syntax
+// console.log(student[anythingIWant]);
+// console.log(student['cohort']);
+
+// dot syntax
+// console.log('does this work?', student.cohort);
+// console['log']('hello');
+
+
