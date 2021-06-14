55b5437d29160646548fc1faeb55095d1f8e5a11
Andy Lindsay <andydlindsay@gmail.com>
2021-06-13 22:39:52 -0700

    updates

--- w08d01/moai-axe-tree/src/components/Result.jsx
+++ w08d01/moai-axe-tree/src/components/Result.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import {genFeedbackMessage} from '../helpers/helpers';
 
-const Result = () => {
+const Result = (props) => {
   const [highScores, setHighScores] = React.useState([]);
 
   const fetchHighScores = () => {
@@ -13,7 +14,7 @@ const Result = () => {
     
   return(
     <footer data-testid="result_footer">
-      <h2>Waiting for your choice!</h2>
+      <h2>{genFeedbackMessage(props.status)}</h2>
       <button onClick={fetchHighScores} data-testid="high-scores">High Scores!</button>
       { highScores.map(({ id, name, points }) => <li key={id}>{name}: {points}</li>) }
     </footer>
